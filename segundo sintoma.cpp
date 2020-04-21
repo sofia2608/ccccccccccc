b374977c168e09875c70b2f383563997758622fe
MiguelT <miguel.ats26@gmail.com>
2020-04-21 03:52:12 -0500

    segunda modificacion

---
+++ segundo sintoma.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	float n,v=37;
+	int t;
+	
+	cout<<"detector del segundo sintoma del covid-19, porfavor digite su temperatura por teclado"<<endl;
+	cin>>n;
+	
+	if(n>=v){
+		cout<<"\ntienes el segundo sintoma del covid-19, llama por un chequeo";
+	}
+	else{
+		cout<<"\ncuidate";
+	}
+	
+	
+	
+	
+	return 0;
+}
