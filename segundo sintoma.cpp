2946fd0666bc0158fee969fc2598034a834458e5
sofia2608 <63891395+sofia2608@users.noreply.github.com>
2020-05-09 18:03:20 -0500

    Update segundo sintoma.cpp

--- segundo sintoma.cpp
+++ segundo sintoma.cpp
@@ -6,14 +6,14 @@ int main(){
 	float n,v=37;
 	int t;
 	
-	cout<<"detector del segundo sintoma del covid-19, porfavor digite su temperatura por teclado"<<endl;
+	cout<<"detector del segundo sintoma del covid-19, porfavor digita su temperatura por teclado"<<endl;
 	cin>>n;
 	
 	if(n>=v){
 		cout<<"\ntienes el segundo sintoma del covid-19, llama por un chequeo";
 	}
 	else{
-		cout<<"\ncuidate";
+		cout<<"\ncuidate mucho y quedate en casa";
 	}
 	
 	return 0;
